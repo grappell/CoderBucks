95ff7fff020f97d228d06a68050700999821479f
grappell <graysonpa@gmail.com>
2021-10-28 17:39:10 -0700

    changes

--- src/routes/organization/[orgName]/teachers/[teacherName]/index.svelte
+++ src/routes/organization/[orgName]/teachers/[teacherName]/index.svelte
@@ -1,50 +1,82 @@
-<script> 
-    import { goto, stores } from '@sapper/app';
-    import { getTeacherSubCollections } from '../../../../../db';
-    import {onMount} from "svelte"
-    import { ListGroup, ListGroupItem, Row, Col } from "sveltestrap/src"
-    import Product from "../../../../../components/product.svelte"
-	const { page } = stores();
+<script>
+  import { goto, stores } from "@sapper/app";
+  import { getTeacherSubCollections } from "../../../../../db";
+  import { onMount } from "svelte";
+  import {
+    Row,
+    FormGroup,
+    Input,
+    Label,
+    Col,
+    Button,
+    ListGroup,
+    ListGroupItem,
+  } from "sveltestrap/src";
+  import Product from "../../../../../components/product.svelte";
+  const { page } = stores();
 
-    $: capitalTeacherName = $page.params.teacherName.charAt(0).toUpperCase() + $page.params.teacherName.substring(1)
+  $: capitalTeacherName =
+    $page.params.teacherName.charAt(0).toUpperCase() +
+    $page.params.teacherName.substring(1);
 
-    var studentList = []
-    var productSuperList = []
-    onMount(async () => {
-        studentList = await getTeacherSubCollections($page.params.orgName, $page.params.teacherName, "students")
-        productSuperList = chunk(3, await getTeacherSubCollections($page.params.orgName, $page.params.teacherName, "products"))
-    })
+  var studentList = [];
+  var productSuperList = [];
+  let cbNum;
+  onMount(async () => {
+    studentList = await getTeacherSubCollections(
+      $page.params.orgName,
+      $page.params.teacherName,
+      "students"
+    );
+    productSuperList = chunk(
+      3,
+      await getTeacherSubCollections(
+        $page.params.orgName,
+        $page.params.teacherName,
+        "products"
+      )
+    );
+  });
 
-    function chunk (chunkSize, list) {
-        var R = [];
-        for (var i = 0; i < list.length; i += chunkSize)
-            R.push(list.slice(i, i + chunkSize));
-        return R;
-    }
+  function chunk(chunkSize, list) {
+    var R = [];
+    for (var i = 0; i < list.length; i += chunkSize)
+      R.push(list.slice(i, i + chunkSize));
+    return R;
+  }
 </script>
 
-<h2> {capitalTeacherName}'s Homepage </h2>
-<br>
+<h2>{capitalTeacherName}'s Homepage</h2>
+<br />
 
 <ListGroup>
-    <h4> Students:</h4>
-    {#each studentList as student}
-    <h4>{student.name}</h4> 
-    <ListGroupItem tag="button"> {student.name}</ListGroupItem>
-    {/each}
+  <h4>Students:</h4>
+  {#each studentList as student}
+    <h3>{student.name}</h3>
+    <ListGroupItem tag="button">{student.name}</ListGroupItem>
+    <FormGroup>
+      <Label for="text">CoderBucks To assign</Label>
+      <Input
+        type="number"
+        name="cbNumInput"
+        id="cbNumInputId"
+        bind:value={cbNum}
+        placeholder="School Name"
+      />
+    </FormGroup>
+  {/each}
 </ListGroup>
 
 <ListGroup>
-    <h4> Products:</h4>
-    
+  <h4>Products:</h4>
 </ListGroup>
 
 {#each productSuperList as productList}
-    <Row>
-        {#each productList as product}
-            <Col md="4">
-                <Product {product}></Product>
-            </Col>
-        {/each}
-    </Row>
+  <Row>
+    {#each productList as product}
+      <Col md="4">
+        <Product {product} />
+      </Col>
+    {/each}
+  </Row>
 {/each}
